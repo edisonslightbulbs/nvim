e4cd7e0721b99cc898225a0aac310e6873d7001b
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2022-06-18 20:45:57 +0200

    adding utils module

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
+require('utils')
 require('mappings')
 
 require('settings')
@@ -8,6 +9,7 @@ require('plugins')
 require('plugins.config.repeat')
 require('plugins.config.airline')
 require('plugins.config.bufonly')
+require('plugins.config.fugitive')
 require('plugins.config.supertab')
 require('plugins.config.nvimtree')
 require('plugins.config.ultisnipps')
@@ -15,6 +17,7 @@ require('plugins.config.floatterm')
 require('plugins.config.telescope')
 require('plugins.config.indentline')
 require('plugins.config.autoformat')
+
 require('plugins.custom.sandbox')
 
 require('color-schemes')
