f7ced207da16760ae8393f5838340519d0fd7548
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2022-06-19 08:19:23 +0200

    ordering airline setup commands

--- lua/plugins/config/airline.lua
+++ lua/plugins/config/airline.lua
@@ -1,20 +1,19 @@
-vim.g.airline_theme = 'jellybeans'
-vim.g.airline_section_c = '%-0.100{getcwd()}'
-
-vim.g.airline_left_sep = ''
-vim.g.airline_right_sep = ''
-
 vim.g.airline_detect_paste = 0
 vim.g.airline_detect_crypt = 1
 vim.g.airline_detect_spell = 0
-
 vim.g.airline_detect_modified = 1
 vim.g.airline_powerline_fonts = 1
-vim.g.airline_inactive_collapse = 0
 
-vim.cmd [[
-   let g:airline#extensions#tabline#enabled = 1
-   let g:airline#extensions#branch#vcs_checks  =  []
-   let g:airline#extensions#tabline#left_sep  =  ''
-   let g:airline#extensions#tabline#left_alt_sep  =  ''
-]]
+vim.cmd [[ let g:airline#extensions#tabline#enabled = 1 ]]
+
+vim.g.airline_inactive_collapse = 0
+vim.g.airline_section_c = '%-0.100{getcwd()}'
+
+vim.cmd [[ let g:airline#extensions#branch#vcs_checks  =  [] ]]
+vim.cmd [[ let g:airline#extensions#tabline#left_sep  =  '' ]]
+vim.cmd [[ let g:airline#extensions#tabline#left_alt_sep  =  '' ]]
+
+vim.g.airline_theme = 'jellybeans'
+
+vim.g.airline_left_sep = ''
+vim.g.airline_right_sep = ''
