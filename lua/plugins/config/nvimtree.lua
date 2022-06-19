3297e0a8a76a726a5bc45d71b04981fd011b6a75
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2022-06-19 15:56:28 +0200

    configuring nvimtree

--- lua/plugins/config/nvimtree.lua
+++ lua/plugins/config/nvimtree.lua
@@ -5,21 +5,62 @@ local opts = { noremap = true, silent = true }
 
 --setup
 require("nvim-tree").setup({
-  sort_by = "case_sensitive",
-  view = {
-    adaptive_size = true,
-    mappings = {
-      list = {
-        { key = "u", action = "dir_up" },
-      },
+    update_cwd = true,
+    sort_by = "case_sensitive",
+    hijack_unnamed_buffer_when_opening = true,
+
+    view = {
+        number = true,
+        relativenumber = true,
+        adaptive_size = true,
+        mappings = {
+            list = {
+                { key = "u", action = "dir_up" },
+            },
+        },
     },
-  },
-  renderer = {
-    group_empty = true,
-  },
-  filters = {
-    dotfiles = false,
-  },
+
+    renderer = {
+        group_empty = true,
+        icons = {
+            glyphs = {
+                default = "",
+                symlink = "",
+                folder = {
+                    arrow_closed = "",
+                    arrow_open = "",
+                    default = "",
+                    open = "",
+                    empty = "",
+                    empty_open = "",
+                    symlink = "",
+                    symlink_open = "",
+                },
+                git = {
+                    unstaged = "",
+                    staged = "",
+                    unmerged = "",
+                    renamed = "",
+                    untracked = "",
+                    deleted = "",
+                    ignored = "",
+                },
+            },
+        },
+    },
+
+    --
+    filters = {
+        dotfiles = false,
+    },
+
+    --
+    update_focused_file = {
+        enable = true,
+        update_cwd = true,
+        update_root = true,
+        ignore_list = {},
+    },
 })
 
 
