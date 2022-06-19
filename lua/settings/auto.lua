ce51ac164fa768252ff5fa4f9c673d697279732a
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2022-06-19 19:28:40 +0200

    commenting out example autogroup

--- lua/settings/auto.lua
+++ lua/settings/auto.lua
@@ -3,42 +3,42 @@ local group = vim.api.nvim_create_augroup("startup", { clear = true })
 vim.api.nvim_create_autocmd("VimEnter", { command = "echo 'Now then, lets get started, shall we?'", group = "startup"})
 
 -- concept
-vim.api.nvim_create_autocmd("FileType", {
-    pattern = { "lua", "text"},
-    callback = function()
-
-        -- my data table
-        local data = {
-            buf = vim.fn.expand("<abuf>"),
-            file = vim.fn.expand("<afile>"),
-            match = vim.fn.expand("<amatch>"),
-        }
-        vim.schedule(function()
-            print("Hey, we go called")
-            print(vim.inspect(data))
-        end)
-    end,
-})
+-- vim.api.nvim_create_autocmd("FileType", {
+--     pattern = { "lua", "text"},
+--     callback = function()
+--
+--         -- my data table
+--         local data = {
+--             buf = vim.fn.expand("<abuf>"),
+--             file = vim.fn.expand("<afile>"),
+--             match = vim.fn.expand("<amatch>"),
+--         }
+--         vim.schedule(function()
+--             print("Hey, we go called")
+--             print(vim.inspect(data))
+--         end)
+--     end,
+-- })
 
 
 
 vim.cmd [[
 " use dir of working buffer
 augroup to_buff_dir
-    autocmd!
-    autocmd BufEnter * silent! lcd %:p:h
+autocmd!
+autocmd BufEnter * silent! lcd %:p:h
 augroup END
 
 " silence pesky read only for git diff
 augroup noro_in_diff
-    autocmd!
-    autocmd BufEnter * if &diff | set noro |endif
+autocmd!
+autocmd BufEnter * if &diff | set noro |endif
 augroup END
 
 " allow external file updates
 augroup check_ext_buff_changes
-    autocmd!
-    autocmd CursorHold,CursorHoldI,FocusGained,BufEnter * checktime
+autocmd!
+autocmd CursorHold,CursorHoldI,FocusGained,BufEnter * checktime
 augroup END
 
 " allow external file updates
@@ -49,8 +49,8 @@ augroup END
 
 " trim white spaces:
 augroup strip_white_spaces
-    autocmd!
-    autocmd BufWritePre * %s/\s\+$//e
+autocmd!
+autocmd BufWritePre * %s/\s\+$//e
 augroup END
 
 " line wrapping:
