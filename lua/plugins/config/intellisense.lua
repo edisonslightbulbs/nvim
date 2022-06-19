0c8bc296f93b712cf9f12a3cb28bb468c7e37d9f
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2022-06-19 19:27:39 +0200

    fixing nvim-lsp-installer & lspconfig

--- lua/plugins/config/intellisense.lua
+++ lua/plugins/config/intellisense.lua
@@ -1,73 +1,31 @@
--- require("nvim-lsp-installer").setup({
---     -- automatically detect which servers to install
---     -- (based on which servers are set up via lspconfig)
---     automatic_installation = true,
---     ui = {
---         icons = {
---             server_installed = "",
---             server_pending = "",
---             server_uninstalled = ""
---         }
---     }
--- })
+-- check nvim-lsp-installer and lspconfig AOT
+local status, installer = pcall(require, "nvim-lsp-installer")
+if not status then
+    print("-- something went wrong while setting up nvim-lsp-installer!")
+    return
+end
 
---
---
---local lspconfig = require("lspconfig")
---local function on_attach(client, bufnr)
---    -- set up buffer keymaps, etc.
---end
+local status, config = pcall(require, "lspconfig")
+if not status then
+    print("-- something went wrong while setting up lspconfig!")
+    return
+end
 
---lspconfig.sumneko_lua.setup { on_attach = on_attach }
 
--- Setup nvim-cmp.
--- local cmp = require'cmp'
+-- setup nvim-lsp-installer and lspconfig
+require("nvim-lsp-installer").setup {
+    automatic_installation = true, -- detect and install servers
+    ui = {
+        icons = {
+            server_installed = "",
+            server_pending = "",
+            server_uninstalled = ""
+        }
+    }
+}
 
--- cmp.setup({
---     mapping = cmp.mapping.preset.insert({
---         ['<C-b>'] = cmp.mapping.scroll_docs(-4),
---         ['<C-f>'] = cmp.mapping.scroll_docs(4),
---         ['<C-Space>'] = cmp.mapping.complete(),
---         ['<C-e>'] = cmp.mapping.abort(),
---         ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
---     }),
---     sources = cmp.config.sources({
---         { name = 'nvim_lsp' },
---         -- { name = 'ultisnips' }, -- For ultisnips users.
---     }, {
---         { name = 'buffer' },
---     })
--- })
---
--- -- Set configuration for specific filetype.
--- cmp.setup.filetype('gitcommit', {
---     sources = cmp.config.sources({
---         { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
---     }, {
---         { name = 'buffer' },
---     })
--- })
---
--- -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
--- cmp.setup.cmdline('/', {
---     mapping = cmp.mapping.preset.cmdline(),
---     sources = {
---         { name = 'buffer' }
---     }
--- })
---
--- -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
--- cmp.setup.cmdline(':', {
---     mapping = cmp.mapping.preset.cmdline(),
---     sources = cmp.config.sources({
---         { name = 'path' }
---     }, {
---         { name = 'cmdline' }
---     })
--- })
---
--- -- Setup lspconfig.
--- local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
--- require('lspconfig')['<sumneko_lua>'].setup {
---     capabilities = capabilities
--- }
+local function on_attach(client, bufnr)
+    print("set up buffer keymaps, etc")
+end
+local lspconfig = require("lspconfig")
+lspconfig.sumneko_lua.setup { on_attach = on_attach }
