1651d46b1930b1c25d67befcaec84246ea9c524c
Everett <everett.mthunzi@zeiss.com>
2022-06-20 15:53:55 +0200

    issue: unable to verify plugins after loading them

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,4 +1,5 @@
 return require('packer').startup(function()
+    -- packer
     use 'wbthomason/packer.nvim'
 
     -- theme
@@ -9,6 +10,7 @@ return require('packer').startup(function()
     use 'vim-airline/vim-airline-themes'
 
     -- editting
+    --use 'SirVer/ultisnips'
     use 'tpope/vim-repeat'
     use 'ervandew/supertab'
     use 'honza/vim-snippets'
@@ -31,37 +33,36 @@ return require('packer').startup(function()
         'nvim-treesitter/nvim-treesitter',
         run = ':TSUpdate'
     }
-    --use 'williamboman/nvim-lsp-installer'
+    use 'williamboman/nvim-lsp-installer'
     use 'neovim/nvim-lspconfig'
     use 'hrsh7th/cmp-nvim-lsp'
     use 'hrsh7th/cmp-buffer'
     use 'hrsh7th/cmp-path'
     use 'hrsh7th/cmp-cmdline'
     use 'hrsh7th/nvim-cmp'
-    --use 'SirVer/ultisnips'
 end)
 
--- verify treesitter plugged correctly
-local status, treesitter = pcall(require, "nvim-treesitter.configs")
-if not status then
-    print("-- something went wrong while setting up treesitter!")
-    return
-end
+-- -- verify treesitter plugged correctly
+-- local status, treesitter = pcall(require, "nvim-treesitter.configs")
+-- if not status then
+--     print("-- something went wrong while setting up treesitter!")
+--     return
+-- end
 -- verify nvim-lsp-installer plugged correctly
-local status, installer = pcall(require, "nvim-lsp-installer")
-if not status then
-    print("-- something went wrong while setting up nvim-lsp-installer!")
-    return
-end
--- verify lspconfig plugged correctly
-local status, config = pcall(require, "lspconfig")
-if not status then
-    print("-- something went wrong while setting up lspconfig!")
-    return
-end
--- verify cmp plugged correctly
-local status, cmp = pcall(require, "cmp")
-if not status then
-    print("-- something went wrong while setting cmp!")
-    return
-end
+-- local status, installer = pcall(require, "nvim-lsp-installer")
+-- if not status then
+--     print("-- something went wrong while setting up nvim-lsp-installer!")
+--     return
+-- end
+---- verify lspconfig plugged correctly
+--local status, config = pcall(require, "lspconfig")
+--if not status then
+--    print("-- something went wrong while setting up lspconfig!")
+--    return
+--end
+---- verify cmp plugged correctly
+--local status, cmp = pcall(require, "cmp")
+--if not status then
+--    print("-- something went wrong while setting cmp!")
+--    return
+--end
