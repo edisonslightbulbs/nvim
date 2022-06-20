881a344b16b6d36dfc14249d473c2a46d2cb152f
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2022-06-20 19:35:23 +0200

    minor lua migration

--- lua/plugins/config/airline.lua
+++ lua/plugins/config/airline.lua
@@ -4,14 +4,14 @@ vim.g.airline_detect_spell = 0
 vim.g.airline_detect_modified = 1
 vim.g.airline_powerline_fonts = 1
 
-vim.cmd [[ let g:airline#extensions#tabline#enabled = 1 ]]
+vim.g['airline#extensions#tabline#enabled'] = 1
 
 vim.g.airline_inactive_collapse = 0
 vim.g.airline_section_c = '%-0.100{getcwd()}'
 
+vim.g['airline#extensions#tabline#left_sep']  =  ''
+vim.g['airline#extensions#tabline#left_alt_sep']  =  ''
 vim.cmd [[ let g:airline#extensions#branch#vcs_checks  =  [] ]]
-vim.cmd [[ let g:airline#extensions#tabline#left_sep  =  '' ]]
-vim.cmd [[ let g:airline#extensions#tabline#left_alt_sep  =  '' ]]
 
 vim.g.airline_theme = 'jellybeans'
 
--- lua/plugins/config/autoformat.lua
+++ lua/plugins/config/autoformat.lua
@@ -3,61 +3,51 @@ vim.cmd [[
 " formatter paths
 "
 if has('win32')
-let g:formatterpath = ['$HOME\AppData\Roaming\npm\', '$HOME\go\bin\',
-            \ 'C:\ProgramData\chocolatey\bin\',
-            \ 'C:\Users\zoemthun\AppData\Local\Programs\Python\Python39\Scripts\',
-            \ 'C:\Users\zoemthun\AppData\Local\Programs\Python\Python38\Scripts\',
-            \ 'C:\Users\zoemthun\AppData\Local\Programs\Python\Python37-32\Scripts\']
+    let g:formatterpath = ['$HOME\AppData\Roaming\npm\', '$HOME\go\bin\',
+    \ 'C:\ProgramData\chocolatey\bin\',
+    \ 'C:\Users\zoemthun\AppData\Local\Programs\Python\Python39\Scripts\',
+    \ 'C:\Users\zoemthun\AppData\Local\Programs\Python\Python38\Scripts\',
+    \ 'C:\Users\zoemthun\AppData\Local\Programs\Python\Python37-32\Scripts\']
 else
-endif
+    endif
 
-"
-" python formatter
-"
-" let g:formatdef_autopep8 = '"autopep8 --in-place --aggressive --aggressive -".(g:DoesRangeEqualBuffer(a:firstline, a:lastline) ? " --range ".a:firstline." ".a:lastline : "")." ".(&textwidth ? "--max-line-length=".&textwidth : "")'
-" let g:formatters_autopep8 = ['autopep8']
+    "
+    " cxx formatter
+    "
+    let g:formatdef_cmake_format = '"cmake-format - --tab-size ".shiftwidth()." ".(&textwidth ? "--line-width=".&textwidth : "")'
+    let g:formatters_cmake = ['cmake_format']
 
-"
-" cxx formatter
-"
-let g:formatdef_cmake_format = '"cmake-format - --tab-size ".shiftwidth()." ".(&textwidth ? "--line-width=".&textwidth : "")'
-let g:formatters_cmake = ['cmake_format']
+    "
+    " cmake formatter
+    "
+    let g:formatdef_clangformat = '"clang-format"'
+    let g:formatters_cpp = ['clangformat']
 
-"
-" cmake formatter
-"
-let g:formatdef_clangformat = '"clang-format"'
-let g:formatters_cpp = ['clangformat']
+    "
+    " sh formatter
+    "
+    let g:formatdef_sh='"shfmt"'
+    let g:formatters_sh=['sh']
 
-"
-" sh formatter
-"
-let g:formatdef_sh='"shfmt"'
-let g:formatters_sh=['sh']
+    "
+    " latex formatter
+    "
+    "let g:formatdef_latexindent = '"latexindent -"'
 
-"
-" zsh formatter
-"
-let g:formatdef_zsh='"shfmt"'
-let g:formatters_zsh=['zsh']
+    "
+    " verbose troubleshooting
+    "
+    let g:autoformat_verbosemode=1
 
-"
-" latex formatter
-"
-"let g:formatdef_latexindent = '"latexindent -"'
-
-"
-" verbose troubleshooting
-"
-let g:autoformat_verbosemode=1
-
-"
-" keybinds
-"
-function! Retab()
+    "
+    " keybinds
+    "
+    function! Retab()
+    let l:view = winsaveview()
     execute ':retab!'
     execute ':Autoformat'
-endfunction
+    call winrestview(l:view)
+    endfunction
 
-nnoremap <Leader>i :call Retab()<CR>
-]]
+    nnoremap <Leader>i :call Retab()<CR>
+    ]]
