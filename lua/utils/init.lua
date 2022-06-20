df5da0f0818f64c9add5bd3a71e08fd73f41bc1f
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2022-06-20 19:42:16 +0200

    adding NextClosedFold(dir) for navigating folds intelligibly

--- lua/utils/init.lua
+++ lua/utils/init.lua
@@ -157,6 +157,24 @@ endfunction
 
 nnoremap <silent><C-O> :call CallFloatTerm()<CR>
 
+nnoremap <silent> <leader>zj :call NextClosedFold('j')<cr>
+nnoremap <silent> <leader>zk :call NextClosedFold('k')<cr>
+
+function! NextClosedFold(dir)
+    let cmd = 'norm!z' . a:dir
+    let view = winsaveview()
+    let [l0, l, open] = [0, view.lnum, 1]
+    while l != l0 && open
+        exe cmd
+        let [l0, l] = [l, line('.')]
+        let open = foldclosed(l) < 0
+    endwhile
+    if open
+        call winrestview(view)
+    endif
+endfunction
+
+
 
 
 ]]
