618531be35fc2cd9cc0f782347a537f361133f8c
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2022-06-23 06:16:32 +0200

    intuitive buffer navigation for when the case when nvimtree is open

---
+++ lua/keybindings/buffer.lua
@@ -0,0 +1,19 @@
+local map =  vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
+
+vim.cmd[[
+function! BuffNav()
+    if len(filter(range(1, bufnr('$')), 'buflisted(v:val)')) > 1
+        let buff = bufname('')
+        execute ':bN'
+        execute ':bd ' . buff
+        echo  buff . ' closed'
+    endif
+endfunction
+]]
+
+map('n', '<S-Tab>', ':bN<CR>', opts)               -- previous buffer
+map('n', '<leader>0', ':edit!', opts)              -- reload buffer << @todo: make auto >>
+map('n', '<leader><Tab>', ':bn<CR>', opts)         -- next buffer
+map('n', '<leader><ENTER>', ':w!<CR>', opts)       -- write buffer
+map('n', '<leader>d', ':call BuffNav()<CR>', opts) -- delete buffer
