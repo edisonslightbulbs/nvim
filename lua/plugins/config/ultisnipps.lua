5eff79c2550eddeb2ba840dda695071b66d4ab6a
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2022-06-24 20:59:25 +0200

    cleaning ultisnipps.lua

--- lua/plugins/config/ultisnipps.lua
+++ lua/plugins/config/ultisnipps.lua
@@ -4,7 +4,6 @@ if not status then
     return
 end
 
-
 require("cmp_nvim_ultisnips").setup {
     filetype_source = "treesitter",
     show_snippets = "all",
@@ -12,36 +11,3 @@ require("cmp_nvim_ultisnips").setup {
         return snippet.description
     end
 }
-
-
-
-
--- local config = function()
---     local cmp_ultisnips_mappings = require("cmp_nvim_ultisnips.mappings")
---     require("cmp").setup({
---         snippet = {
---             expand = function(args)
---                 vim.fn["UltiSnips#Anon"](args.body)
---             end,
---         },
---         sources = {
---             { name = "ultisnips" },
---             -- more sources
---         },
---         -- recommended configuration for <Tab> people:
---         mapping = {
---             ["<Tab>"] = cmp.mapping(
---             function(fallback)
---                 cmp_ultisnips_mappings.expand_or_jump_forwards(fallback)
---             end,
---             { "i", "s", --[[ "c" (to enable the mapping in command mode) ]] }
---             ),
---             ["<S-Tab>"] = cmp.mapping(
---             function(fallback)
---                 cmp_ultisnips_mappings.jump_backwards(fallback)
---             end,
---             { "i", "s", --[[ "c" (to enable the mapping in command mode) ]] }
---             ),
---         },
---     })
--- end
