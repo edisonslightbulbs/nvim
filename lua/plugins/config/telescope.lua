a6b351b6365a890c49844351efcc3eb364392116
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2022-06-25 18:56:30 +0200

    telescope config: ignoring .gitignore files

--- lua/plugins/config/telescope.lua
+++ lua/plugins/config/telescope.lua
@@ -1,6 +1,14 @@
--- aliases
+local status, ret = pcall(require, "telescope")
+if not status then
+    print("-- something went wrong while setting up telescope!")
+    return
+end
+
+require('telescope').setup{ }
+
 local map =  vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 
-map('n', '<leader><Space>', ':Telescope git_files <CR>', opts) -- pull up telescope
+--map('n', '<leader><Space>', ':Telescope git_files <CR>', opts) -- pull up telescope
 --map('n', '<leader>', ':Telescope find_files <CR>', opts) -- pull up telescope
+map('n', '<leader><Space>', '<cmd>lua require("telescope.builtin").find_files({ no_ignore = true })<CR>', opts)
