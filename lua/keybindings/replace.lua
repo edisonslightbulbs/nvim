4837865f4e4af747ab151a074b6f8e558e225419
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2022-06-25 09:48:32 +0200

    minor tweak: holding the view as opposed to being left at the bottom of a search/window

--- lua/keybindings/replace.lua
+++ lua/keybindings/replace.lua
@@ -1,14 +1,18 @@
 vim.cmd[[
 function! ReplaceWordFunction(arg)
+    let l:view = winsaveview()
     execute '%s/' . expand('<cword>') . '/' . a:arg . '/gc'
+    call winrestview(l:view)
 endfunction
 command! -nargs=* Repword :call ReplaceWordFunction(<f-args>)
 
 function! ReplaceWORDFunction(arg)
+    let l:view = winsaveview()
     execute '%s/' . expand('<cWORD>') . '/' . a:arg . '/gc'
+    call winrestview(l:view)
 endfunction
 command! -nargs=* RepWORD :call ReplaceWORDFunction(<f-args>)
 
 nnoremap <Leader>r  :Repword
-nnoremap <Leader>R  :RepWORD 
+nnoremap <Leader>R  :RepWORD
 ]]
