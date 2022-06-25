4d8c2abecc60686187dcc99241fa7cfa0aaead43
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2022-06-25 17:51:44 +0200

    adding a naive tweak to nvimtree

--- lua/plugins/config/nvimtree.lua
+++ lua/plugins/config/nvimtree.lua
@@ -1,71 +1,79 @@
 -- aliases
-local map =  vim.api.nvim_set_keymap
-local opts = { noremap = true, silent = true }
+local map = vim.api.nvim_set_keymap
+local opts = {noremap = true, silent = true}
 
-require("nvim-tree").setup({
-    update_cwd = true,
-    sort_by = "case_sensitive",
-    hijack_unnamed_buffer_when_opening = true,
-    hijack_netrw = true,
-
-    view = {
-        number = true,
-        relativenumber = true,
-        adaptive_size = true,
-        mappings = {
-            list = {
-                { key = "u", action = "dir_up" },
-            },
+require("nvim-tree").setup(
+    {
+        view = {
+            number = true,
+            relativenumber = true,
+            adaptive_size = true,
+            mappings = {
+                list = {
+                    {key = "u", action = "dir_up"}
+                }
+            }
         },
-    },
-
-    renderer = {
-        group_empty = true,
-        icons = {
-            glyphs = {
-                default = "",
-                symlink = "",
-                folder = {
-                    arrow_closed = "",
-                    arrow_open = "",
-                    default = "",
-                    open = "",
-                    empty = "",
-                    empty_open = "",
-                    symlink = "",
-                    symlink_open = "",
-                },
-                git = {
-                    unstaged = "",
-                    staged = "",
-                    unmerged = "",
-                    renamed = "",
-                    untracked = "",
-                    deleted = "",
-                    ignored = "",
-                },
-            },
+        renderer = {
+            group_empty = true,
+            icons = {
+                glyphs = {
+                    default = "",
+                    symlink = "",
+                    folder = {
+                        arrow_closed = "",
+                        arrow_open = "",
+                        default = "",
+                        open = "",
+                        empty = "",
+                        empty_open = "",
+                        symlink = "",
+                        symlink_open = ""
+                    },
+                    git = {
+                        unstaged = "",
+                        staged = "",
+                        unmerged = "",
+                        renamed = "",
+                        untracked = "",
+                        deleted = "",
+                        ignored = ""
+                    }
+                }
+            }
         },
-    },
+        filters = {
+            dotfiles = false,
+            exclude = {"*.metainfo", "*.py.lock"}
+        },
+        update_focused_file = {
+            enable = false,
+            update_cwd = true,
+            update_root = true,
+            ignore_list = {}
+        },
+        git = {
+            enable = false,
+            ignore = false,
+            timeout = 1
+        }
+    }
+)
 
-    filters = {
-        dotfiles = false,
-        exclude = {"*.metainfo", "*.py.lock"},
-    },
+vim.cmd[[
+function! NvimTreeStart()
+if IsNoName()
+    let l:buff = bufname('')
+    silent! execute ':NvimTreeOpen'
+    silent! execute ':BufOnly'
+endif
+endfunction
 
-    update_focused_file = {
-        enable = false,
-        update_cwd = true,
-        update_root = true,
-        ignore_list = {},
-    },
+augroup nvimtree_startup_au
+    autocmd!
+    autocmd VimEnter * :call NvimTreeStart ()
+augroup END
 
-    git = {
-        enable = false,
-        ignore = false,
-        timeout = 1,
-    },
+]]
 
-})
-
-map('n', '<leader>t', ':NvimTreeToggle<CR>', opts) -- toggle
+map("n", "<leader>t", ":NvimTreeToggle<CR>", opts) -- toggle
