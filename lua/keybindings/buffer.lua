78884b60ef967e3464bc3bbec5a37b4f6a05b366
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2022-06-25 18:54:08 +0200

    unfolding on BufEnter

--- lua/keybindings/buffer.lua
+++ lua/keybindings/buffer.lua
@@ -8,6 +8,8 @@ function! BuffNav()
         execute ':bN'
         execute ':bd ' . buff
         echo  buff . ' closed'
+    else
+        execute ':bd!'
     endif
 endfunction
 ]]
@@ -16,3 +18,14 @@ map('n', '<S-Tab>', ':bN<CR>', opts)               -- previous buffer
 map('n', '<leader>0', ':edit!', opts)              -- reload buffer << @todo: make auto >>
 map('n', '<leader><Tab>', ':bn<CR>', opts)         -- next buffer
 map('n', '<leader>d', ':call BuffNav()<CR>', opts) -- delete buffer
+
+vim.cmd[[
+
+augroup unfold_au
+    autocmd!
+    autocmd BufWinEnter * silent! :%foldopen!
+    " autocmd BufRead * normal zR
+augroup END
+]]
+
+map("n", "<leader>t", ":NvimTreeToggle<CR>", opts) -- toggle
