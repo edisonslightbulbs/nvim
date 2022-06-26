ce9a101b84d2c28f9ee614ab31bbc718db5d579c
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2022-06-26 08:08:41 +0200

    @todo adding telescope keybinds

--- lua/plugins/config/telescope.lua
+++ lua/plugins/config/telescope.lua
@@ -11,4 +11,5 @@ local opts = { noremap = true, silent = true }
 
 --map('n', '<leader><Space>', ':Telescope git_files <CR>', opts) -- pull up telescope
 --map('n', '<leader>', ':Telescope find_files <CR>', opts) -- pull up telescope
-map('n', '<leader><Space>', '<cmd>lua require("telescope.builtin").find_files({ no_ignore = true })<CR>', opts)
+--map('n', '<leader><Space>', '<cmd>lua require("telescope.builtin").find_files({ no_ignore = true })<CR>', opts)
+map('n', '<leader><Space>', '<cmd>lua require("telescope.builtin").git_files({ no_ignore = true })<CR>', opts)
