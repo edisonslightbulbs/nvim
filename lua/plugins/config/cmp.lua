31a3638603dbc9d937456c0721729d3fc39ca33b
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2022-06-29 07:27:18 +0200

    cmp config

--- lua/plugins/config/cmp.lua
+++ lua/plugins/config/cmp.lua
@@ -4,19 +4,26 @@ if not status then
     return
 end
 
+status, ret = pcall(require, "lspkind")
+if not status then
+    print("-- something went wrong while setting upl spkind!")
+    return
+end
+
 local cmp = require'cmp'
+local lspkind = require('lspkind')
 cmp.setup({
     snippet = {
-        -- snippet engines
         expand = function(args)
-            --require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
             vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
         end,
     },
+
     window = {
         completion = cmp.config.window.bordered(),
         documentation = cmp.config.window.bordered(),
     },
+
     mapping = cmp.mapping.preset.insert({
         ['<S-Tab>'] = cmp.mapping.select_prev_item(),
         ['<Tab>'] = cmp.mapping.select_next_item(),
@@ -26,51 +33,59 @@ cmp.setup({
         --['<ESCAPE>'] = cmp.mapping.abort(),
         ['<C-Space>'] = cmp.mapping.confirm({ select = true }),
     }),
+
     sources = cmp.config.sources({
-        --{ name = 'luasnip' },
+        { name = 'nvim_lsp' },
+        { name = 'path' },
+        --{ name = 'git' },
+        { name = 'nvim_lua' },
         { name = 'ultisnips' },
-    }, {
-        { name = 'buffer' },
-    })
-})
+        { name = 'buffer', keyword_length = 5 },
+    }),
 
-
--- Set configuration for specific filetype.
-cmp.setup.filetype('gitcommit', {
-    sources = cmp.config.sources({
-        { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
-    }, {
-        { name = 'buffer' },
-    })
-})
-
--- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline('/', {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = {
-        { name = 'buffer' }
-    }
-})
-
--- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline(':', {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = cmp.config.sources({
-        { name = 'path' }
-    }, {
-        { name = 'cmdline' }
-    })
-})
-
--- Setup lspconfig.
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
-require('lspconfig')['sumneko_lua'].setup {
-    capabilities = capabilities,
-    settings = {
-        Lua = {
-            diagnostics = {
-                globals = {"vim"},
+    formatting = {
+        format = lspkind.cmp_format({
+            mode = 'symbol_text',
+            menu = {
+                nvim_lsp = "[lsp]",
+                path = "[path]",
+                git = "[git]",
+                nvim_lua = "[lua]",
+                ultisnips = "[snipp]",
+                buffer = "[buf]",
             },
+            -- maxwidth = 50,
+            --before = function (entry, vim_item)
+                --    return vim_item
+                --end
+                symbol_map = {
+                    Text = "",
+                    Method = "",
+                    Function = "",
+                    Constructor = "",
+                    Field = "ﰠ",
+                    Variable = "",
+                    Class = "ﴯ",
+                    Interface = "",
+                    Module = "",
+                    Property = "ﰠ",
+                    Unit = "塞",
+                    Value = "",
+                    Enum = "",
+                    Keyword = "",
+                    Snippet = "",
+                    Color = "",
+                    File = "",
+                    Reference = "",
+                    Folder = "",
+                    EnumMember = "",
+                    Constant = "",
+                    Struct = "פּ",
+                    Event = "",
+                    Operator = "",
+                    TypeParameter = ""
+                },
+            })
         },
-    },
-}
+    })
+
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -30,16 +30,15 @@ local function plug()
         use 'nvim-treesitter/nvim-treesitter'
         use 'williamboman/nvim-lsp-installer'
         use 'neovim/nvim-lspconfig'
-        use 'hrsh7th/cmp-nvim-lsp'
-        use 'hrsh7th/cmp-cmdline'
-        use 'hrsh7th/cmp-buffer'
-        use 'hrsh7th/cmp-path'
         use 'hrsh7th/nvim-cmp'
+        use 'hrsh7th/cmp-path'
+        use 'hrsh7th/cmp-buffer'
+        use 'hrsh7th/cmp-cmdline'
+        use 'hrsh7th/cmp-nvim-lsp'
+        use 'onsails/lspkind.nvim'
         use 'SirVer/ultisnips'
         use 'honza/vim-snippets'
         use 'quangnguyen30192/cmp-nvim-ultisnips'
-        -- use 'L3MON4D3/LuaSnip'
-        -- use 'rafamadriz/friendly-snippets'
     end)
 end
 
