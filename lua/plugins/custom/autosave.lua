9cbcb241f6dc2388e53886c2047396098e628e9d
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2022-07-03 11:07:58 +0200

    lua migration

--- lua/plugins/custom/autosave.lua
+++ lua/plugins/custom/autosave.lua
@@ -1,77 +1,49 @@
-vim.cmd [[
-""" if exists('g:load_autosave') | finish | endif
-""" if exists('g:load_undo') | finish | endif
+-- autosave feature
+local load_autosave = 1
 
-let g:load_undo = 1
-let g:load_autosave = 1
+-- activates autosave plugin
+function AutoSave()
+    if IsSavable() and load_autosave == 1 then
+        local view = vim.fn.winsaveview()
+        vim.api.nvim_command("%s/\\s\\+$//e")
+        vim.fn.winrestview(view)
+        vim.api.nvim_command("write")
+    end
+end
 
-function! IsNvimTree_1()
-    """ checks if current buffer is a NvimTree_1 buffer
-    """
-    if  &filetype ==#'NvimTree_1'
-        return v:true
-    else
-        return v:false
-    endif
-endfunction
-
-function! Savable()
-    """ checks if current buffer is a working buffer.
-    """
-    if Writable() && !IsNvimTree_1()
-        if !IsNoName()
-            return v:true
-        endif
-    else
-        return v:false
-    endif
-endfunction
-
-function! AutoSave()
-    """ autoSave
-    """
-    if Savable() && g:load_autosave ==1
-        let l:view = winsaveview()
-        silent! execute '%s/\s\+$//e'
-        call winrestview(l:view)
-        silent! execute 'write'
-    endif
-endfunction
+local autosave = vim.api.nvim_create_augroup("AutoSave", {clear = true})
+vim.api.nvim_create_autocmd(
+    {"TextChanged", "InsertLeave", "BufLeave", "VimLeavePre"},
+    {
+        pattern = "*",
+        group = autosave,
+        desc = "autosaves work",
+        callback = function()
+            AutoSave()
+        end
+    }
+)
 
 
-function! DisableAutoSave()
-    """ disables autosave plugin
-    """
-    if &readonly
-        let g:load_autosave = 0
-        set nobackup
-        set noswapfile
-        set noundofile
-    endif
-endfunction
+-- deactivates autosave plugin
+function NoAutoSave()
+    load_autosave = 0
+    vim.opt.backup = false
+    vim.opt.writebackup = false
+    vim.opt.swapfile = false
+    vim.api.nvim_command("noundofile")
+end
 
-" setup undodir and undofile
-if has('persistent_undo')
-    let undo_dir = expand('~/.config/vim-persisted-undo/')
-    if !isdirectory(undo_dir)
-        call system('mkdir -p ' . undo_dir)
-    endif
-    let &undodir = undo_dir
-    set undofile
-endif
+-- augroup noautosave_au
+-- autocmd!
+-- autocmd BufReadPost * : call NoAutoSave()
+-- augroup END
 
-augroup autosave_au
-    autocmd!
-    " autosave cases:
-    "   a. after text is changed in normal mode [ TextChanged ]
-    "   b. when leaving insert mode [ InsertLeave ]
-    "   c. before leaving a buffer [ BufLeave ]
-    "   d. before exiting vim [ VimLeavePre ]
-    autocmd TextChanged,InsertLeave,BufLeave,VimLeavePre *  :call AutoSave()
-augroup END
 
-augroup mansave_au
-    autocmd!
-    autocmd BufReadPost * : call DisableAutoSave()
-augroup END
-]]
+local undodir = JoinPath(vim.fn.stdpath("config"), "autosave", "undo")
+if vim.fn.isdirectory(undodir) == 0 then
+    vim.fn.system {"mkdir", "-p", undodir}
+end
+
+vim.opt.undodir = undodir
+vim.opt.undofile = true
--- lua/utilities/init.lua
+++ lua/utilities/init.lua
@@ -1,25 +1,22 @@
--- local file = vim.api.nvim_buf_get_name(0)
--- local nr = vim.api.nvim_get_current_buf()
-
+-- joins paths (cross platform)
 function JoinPath(...)
-    -- cross platform paths
-    local sep = ''
+    local sep = ""
     if vim.fn.has("unix") == 1 then
-        sep = '/'
+        sep = "/"
     elseif vim.fn.has("win32") == 1 then
-        sep = '\\'
+        sep = "\\"
     end
     local joined = table.concat({...}, sep)
     return joined
 end
 
+-- checks for an empty str
 local function empty(str)
-    -- checks for an empty str
     return str == nil or str == ""
 end
 
+-- checks for a [No Name] buf
 function IsNoname()
-    -- checks for a [No Name] buf
     if vim.bo.modifiable and empty(vim.bo.buftype) and empty(vim.fn.expand("%")) and empty(vim.bo.filetype) then
         return true
     else
@@ -27,54 +24,69 @@ function IsNoname()
     end
 end
 
+-- checks if current tab is the last tab
+function IsLastTab()
+    return true
+end
 
-vim.cmd [[
-function! IsLastWindow()
-    """ Checks if current window is last window
-    """
-    if winnr() == winnr('$')
-        return v:true
+-- checks if current win is the last window
+function IsLastWin()
+    if IsLastTab() then
+        if vim.fn.winnr() == vim.fn.winnr("$") then
+            return true
+        else
+            return false
+        end
     else
-        return v:false
-    endif
-endfunction
+        return false
+    end
+end
 
-function! IsLastBuffer()
-    """ Verifies current buffer is last buffer
-    """
-    let l:buffer_count = 0
-    for i in range(0, bufnr('$'))
-        if buflisted(i)
-            let l:buffer_count = l:buffer_count + 1
-        endif
-    endfor
+-- checks if current buf is the last buf [@todo: buggy implementation]
+function IsLastBuf()
+    if IsLastWin() then
+        local bufcount = 0
+        for buf = 0, vim.fn.bufnr("$"), 1 do
+            if vim.fn.buflisted(buf) == 1 and vim.fn.bufloaded(buf) == 1 and vim.fn.empty(vim.fn.win_findbuf(buf)) == 1 then
+                bufcount = bufcount + 1
+            end
+        end
 
-    if l:buffer_count == 1
-        return v:true
+        if bufcount <= 1 then
+            print("this is the last buffer")
+            return true
+        else
+            print("bufcount: " .. bufcount .. " ...this is not the last buffer!!")
+            return false
+        end
     else
-        return v:false
-    endif
-endfunction
+        return false
+    end
+end
 
-function! Writable()
-    """ checks if current buffer is writable
-    """
-    if &modifiable==#'1' && &buftype==#'' && &filetype !=#''
-        return v:true
-    elseif &modifiable!=#'1' || &buftype!=#'' || &filetype ==#''
-        return v:false
-    endif
-endfunction
-
-""""""""""" functions migrated to luascript """""""""""
-
-function! IsNoName()
-    """ checks if current buffer is a No Name buffer
-    """
-    if &buftype==#'' && bufname('%')==#'' && &modifiable==#'1'
-        return v:true
+function IsWritable()
+    -- checks if current buffer is writable
+    if vim.bo.modifiable and empty(vim.bo.buftype) and not empty(vim.bo.filetype) then
+        return true
     else
-        return v:false
-    endif
-endfunction
-]]
+        return false
+    end
+end
+
+function IsNvimTree()
+    -- checks if current buffer NvimTree
+    if vim.bo.filetype == "NvimTree" then
+        return true
+    else
+        return false
+    end
+end
+
+function IsSavable()
+    -- checks if current buffer is a working/savable buffer
+    if IsWritable() and not IsNvimTree() and not IsNoname() then
+        return true
+    else
+        return false
+    end
+end
