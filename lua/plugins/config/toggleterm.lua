8b159cc0f9b63a07f4263ac3b754ee185489e165
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2022-07-11 21:19:21 +0200

    fixing toggleterm config

--- lua/plugins/config/toggleterm.lua
+++ lua/plugins/config/toggleterm.lua
@@ -4,31 +4,17 @@ if not status then
     return
 end
 
-local Terminal = require("toggleterm.terminal").Terminal
-local zsh = Terminal:new({cmd = "source ~/.zshrc", hidden = true})
-
--- description
-_G.toggle_zsh = function()
-    if vim.fn.has("unix") then
-        zsh:toggle()
-    end
-end
-
 require("toggleterm").setup {
     open_mapping = [[<c-o>]],
     direction = "float",
     hide_numbers = true,
     close_on_exit = true,
+    insert_mappings = true,
     terminal_mappings = true,
-    --shell = vim.o.shell
+    shell = "zsh"
     -- shade_filetypes = {},
     -- start_in_insert = false,
     -- insert_mappings = true,
     -- persist_size = true,
     -- persist_mode = true,
 }
-
-local map = vim.api.nvim_set_keymap
-local opts = {noremap = true, silent = true}
-
--- source
