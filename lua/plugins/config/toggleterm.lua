5d87d087f0bf4973a5dbcca05d0bf19f0f98c508
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2022-07-11 21:30:17 +0200

    os watchdog

--- lua/plugins/config/toggleterm.lua
+++ lua/plugins/config/toggleterm.lua
@@ -4,14 +4,21 @@ if not status then
     return
 end
 
+local function term()
+    if vim.fn.has("unix") then
+        return "zsh"
+    end
+    return ""
+end
+
 require("toggleterm").setup {
+    shell = term(),
     open_mapping = [[<c-o>]],
     direction = "float",
     hide_numbers = true,
     close_on_exit = true,
     insert_mappings = true,
-    terminal_mappings = true,
-    shell = "zsh"
+    terminal_mappings = true
     -- shade_filetypes = {},
     -- start_in_insert = false,
     -- insert_mappings = true,
