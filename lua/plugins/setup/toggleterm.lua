600e0191db553b4c4e6ec53fb82736356f6e8d0c
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2022-07-14 07:48:41 +0200

    todo: add global bufpath and bufdir util functions

--- lua/plugins/setup/toggleterm.lua
+++ lua/plugins/setup/toggleterm.lua
@@ -1,29 +1,49 @@
 local status, toggleterm = pcall(require, 'toggleterm')
 if not status then
-    print('-- something went wrong while setting toggleterm!')
-    return
+	print('-- something went wrong while setting toggleterm!')
+	return
 end
 
 local function zsh_term()
-    local os = vim.loop.os_uname().sysname
-    if os == 'Linux' then
-        return 'zsh'
-    elseif os == 'Windows_NT' then
-        return vim.o.shell
-    end
+	local os = vim.loop.os_uname().sysname
+	if os == 'Linux' then
+		return 'zsh'
+	elseif os == 'Windows_NT' then
+		return vim.o.shell
+	end
 end
 
 toggleterm.setup({
-    shell = zsh_term(),
-    open_mapping = [[<c-o>]],
-    direction = 'float',
-    hide_numbers = true,
-    close_on_exit = false,
-    insert_mappings = true,
-    start_in_insert = false,
-    terminal_mappings = true,
-    -- shade_filetypes = {},
-    -- insert_mappings = true,
-    -- persist_size = true,
-    -- persist_mode = true,
+	shell = zsh_term(),
+	--open_mapping = [[<c-o>]],
+	direction = 'float',
+	hide_numbers = true,
+	close_on_exit = false,
+	insert_mappings = true,
+	start_in_insert = false,
+	terminal_mappings = true,
+	-- shade_filetypes = {},
+	-- insert_mappings = true,
+	-- persist_size = true,
+	-- persist_mode = true,
 })
+
+local map = vim.api.nvim_set_keymap
+local opt = { noremap = true, silent = true }
+
+local dir = vim.fn.getcwd()
+local toggle = ':ToggleTerm size=40 dir='..dir..' direction=float<CR>'
+
+print ()
+
+-- todo: make sep global
+local bufpath =vim.api.nvim_buf_get_name(0)
+local sep = package.config:sub(1, 1)
+
+function Z (str)
+    return str:match("(.*"..sep..")")
+end
+
+print(Z(bufpath))
+
+map('n', '<c-o>', toggle, opt)
