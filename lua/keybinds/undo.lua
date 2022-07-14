17b9687656a6a4cc074f230e826063902e694508
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2022-07-14 18:43:27 +0200

    review: minor fix

--- lua/keybinds/undo.lua
+++ lua/keybinds/undo.lua
@@ -1,4 +1,4 @@
-vim.cmd [[
+vim.cmd([[
 function! Undo()
     let l:view = winsaveview()
     execute ':undo'
@@ -6,4 +6,4 @@ function! Undo()
     call winrestview(l:view)
 endfunction
 map('n', '<leader>u', ':call Undo()<CR>', opts)       -- write buffer
-]]
+]])
