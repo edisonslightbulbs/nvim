b98cd9b218102fcd119387cb32832ffcac640448
Everett <everett.mthunzi@zeiss.com>
2022-07-20 06:46:49 +0200

    migration: vimscript to lua

--- lua/settings/hosts.lua
+++ lua/settings/hosts.lua
@@ -1,10 +1,8 @@
-vim.cmd([[
-if has('win32')
-    let g:python3_host_prog = 'C:\Users\zoemthun\AppData\Local\Programs\Python\Python37-32\python.exe'
-    let g:python_host_prog = 'C:\Users\zoemthun\AppData\Local\Microsoft\WindowsApps\python.exe'
-    let g:ruby_host_prog = 'C:\Ruby30-x64\bin\ruby.exe'
-else
-    let g:python3_host_prog='/usr/bin/python3.8'
-    let g:python_host_prog='/usr/bin/python'
-endif
-]])
+if vim.fn.has('unix') then
+    vim.g.python3_host_prog = '/usr/bin/python3.8'
+    vim.g.python_host_prog = '/usr/bin/python'
+elseif vim.fn.has('win32') then
+    vim.g.python3_host_prog = 'C:\\Users\\zoemthun\\AppData\\Local\\Programs\\Python\\Python37-32\\python.exe'
+    vim.g.python_host_prog = 'C:\\Users\\zoemthun\\AppData\\Local\\Microsoft\\WindowsApps\\python.exe'
+    vim.g.ruby_host_prog = 'C:\\Ruby30-x64\\bin\\ruby.exe'
+end
