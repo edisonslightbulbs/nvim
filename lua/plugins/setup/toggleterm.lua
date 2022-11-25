9a8e6ffef2e2514c049509780185e0d1758399cd
Everett <everett.mthunzi@zeiss.com>
2022-11-25 13:45:32 +0100

    config: toggleterm

--- lua/plugins/setup/toggleterm.lua
+++ lua/plugins/setup/toggleterm.lua
@@ -13,18 +13,32 @@ local function zsh_term()
 	end
 end
 
+
 toggleterm.setup({
 	shell = zsh_term(),
-	direction = 'float',
-	hide_numbers = true,
+	direction = 'horizontal',
+	hide_numbers = false,
 	close_on_exit = false,
 	insert_mappings = true,
 	start_in_insert = false,
 	terminal_mappings = true,
+    autochdir = false,
+    auto_scroll = false,
 })
+
+function _G.set_terminal_keymaps()
+  local opts = {buffer = 0}
+  vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+  vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
+  vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+  vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+  vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+  vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+end
+vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
 
 local map = vim.api.nvim_set_keymap
 local opt = { noremap = true, silent = true }
 
-local toggle = ':ToggleTerm size=40 dir=' .. bufdir() .. ' direction=float<CR>'
+local toggle = ':ToggleTerm size=50 dir=' .. bufdir() .. ' insert_mappings=true direction=horizontal<CR> '
 map('n', '<c-o>', toggle, opt)
