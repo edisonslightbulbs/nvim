7a903b5b09111fcebb91454fbb644dde528f719c
Everett <everett.mthunzi@zeiss.com>
2022-11-25 12:11:16 +0100

    config: api deprecation

--- lua/plugins/setup/lspconfig.lua
+++ lua/plugins/setup/lspconfig.lua
@@ -10,7 +10,7 @@ if not cmp_nvim_lsp_status then
 	return
 end
 
-local capabilities = cmp_nvim_lsp.update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = cmp_nvim_lsp.default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 -- keybinds
 local function on_attach(client, bufnr)
