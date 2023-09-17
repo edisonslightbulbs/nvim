31e19e537f30714c4bc128a6fe44ac7f8c0ee01d
Everett <everett.mthunzi@zeiss.com>
2023-09-17 08:01:22 +0200

    dev: passing config file to cmake-format

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -4,14 +4,25 @@ if not status then
 	return
 end
 
+local cmake_config_file_path = config.path.join(vim.fn.stdpath("config"), "formatting", ".cmake-format.yaml")
+
 conform.setup({
+	lazy = true,
 	formatters_by_ft = {
-		python = { "isort", "autopep8" },
+		python = { "isort", "yapf" },
 		lua = { "stylua" },
 		cmake = { "cmake_format" },
 		cpp = { "clang_format" },
 		latex = { "latexindent" },
 		yaml = { "yamlfmt" },
+	},
+	formatters = {
+		cmake_format = {
+			command = "cmake-format", -- The cmake-format command
+			args = { "-c", cmake_config_file_path, "-i", "$FILENAME" }, -- Point to your .cmake-format.yaml file
+			stdin = false, -- Send file contents to stdin
+			exit_codes = { 0 }, -- Exit code that indicates success
+		},
 	},
 })
 
@@ -31,23 +42,3 @@ local map = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 map("n", "<leader>i", ":Format<CR>", opts)
 map("x", "<leader>i", ":Format<CR>", opts)
-
-local util = require("conform.util")
----@type conform.FileFormatterConfig
-return {
-	meta = {
-		url = "https://github.com/psf/black",
-		description = "The uncompromising Python code formatter.",
-	},
-	command = "black",
-	args = {
-		"--stdin-filename",
-		"$FILENAME",
-		"--quiet",
-		"-",
-	},
-	cwd = util.root_file({
-		-- https://black.readthedocs.io/en/stable/usage_and_configuration/the_basics.html#configuration-via-a-file
-		"pyproject.toml",
-	}),
-}
