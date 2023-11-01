2e07f363aa238b08f9b5c07b9b841b1fe4f60e5b
Everett <everett.mthunzi@zeiss.com>
2023-11-01 21:24:48 +0100

    beta: conform settings

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -4,14 +4,89 @@ if not status then
 	return
 end
 
-local cmake_config_file_path = config.path.join(vim.fn.stdpath("config"), "formatting", ".cmake-format.yaml")
-local clang_config_file_path = config.path.join(vim.fn.stdpath("config"), "formatting", ".clang-format.yaml")
+_G.clang_config_file_path = config.path.join(vim.fn.stdpath("config"), "formatting", ".clang-format")
+_G.cmake_config_file_path = config.path.join(vim.fn.stdpath("config"), "formatting", ".cmake-format.yaml")
+
+if config.path.exists(clang_config_file_path) == false then
+    error("Unable to resolve " .. clang_config_file_path)
+end
+
+if config.path.exists(cmake_config_file_path) == false then
+    error("Unable to resolve " .. cmake_config_file_path)
+end
+
+
+_G.setup_clang_format_file = function()
+    local filepath = vim.fn.expand("%:p")
+    local target_dir = vim.fn.fnamemodify(filepath, ":h")
+    local target_file_path = target_dir .. "/.clang-format"
+
+    local file = io.open(target_file_path, "r")
+    if file == nil then
+        local source, src_err = io.open(clang_config_file_path, "rb")
+        local destination, dest_err = io.open(target_file_path, "wb")
+
+        if source and destination then
+            destination:write(source:read("*a"))
+            source:close()
+            destination:close()
+
+            -- Cross-platform permission handling
+            local is_windows = package.config:sub(1,1) == '\\'
+            local cmd = ""
+            if is_windows then
+                cmd = "icacls " .. target_file_path .. " /grant Everyone:R"
+            else
+                cmd = "chmod 644 " .. target_file_path
+            end
+
+            local success, errmsg = pcall(function() os.execute(cmd) end)
+            if not success then
+                vim.api.nvim_err_writeln("Failed to set permissions: " .. errmsg)
+            end
+        else
+            if not source then
+                vim.api.nvim_err_writeln("Failed to open source file: " .. src_err)
+            end
+            if not destination then
+                vim.api.nvim_err_writeln("Failed to open destination file: " .. dest_err)
+            end
+        end
+    else
+        file:close()
+    end
+end
+
+vim.cmd("autocmd BufEnter *.cpp,*.h lua _G.setup_clang_format_file()")
+
+
+_G.cleanup_clang_format_file = function()
+    local filepath = vim.fn.expand("%:p")
+    local target_dir = vim.fn.fnamemodify(filepath, ":h")
+    local target_file_path = target_dir .. "/.clang-format"
+
+    local file = io.open(target_file_path, "r")
+    if file ~= nil then
+        file:close()
+
+        local success, errmsg = pcall(vim.loop.fs_unlink, target_file_path)
+        if not success then
+            vim.api.nvim_err_writeln("Failed to remove " .. target_file_path .. ": " .. errmsg)
+        end
+    end
+end
+
+vim.cmd("autocmd VimLeave,BufUnload *.cpp,*.h lua _G.cleanup_clang_format_file()")
+
+
 
 conform.setup({
 	lazy = true,
 	formatters_by_ft = {
 		python = { "autopep8", "isort"},
 		lua = { "stylua" },
+		cpp = { "clang_format" },
+		h = { "clang_format" },
 		cmake = { "cmake_format" },
 		clangd = { "clang_format" },
 		latex = { "latexindent" },
@@ -29,7 +104,7 @@ conform.setup({
 
 		clang_format = {
 			command = "clang-format",
-			args = { "--style="..cmake_config_file_path, "-i", "$FILENAME" },
+			args = { "--style=file", "-i", "$FILENAME" },
 			stdin = false,
 			exit_codes = { 0 },
 		},
@@ -59,7 +134,7 @@ vim.api.nvim_create_user_command("Format", function(args)
 			["end"] = { args.line2, end_line:len() },
 		}
 	end
-	conform.format({ async = true, lsp_fallback = true, range = range })
+	conform.format({ async = false, lsp_fallback = true, range = range })
 end, { range = true })
 
 local map = vim.api.nvim_set_keymap
